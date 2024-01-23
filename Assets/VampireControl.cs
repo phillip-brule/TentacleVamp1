438907f3125fd96749035ef432938d45fc15dd6d
phillip-brule <55218796+phillip-brule@users.noreply.github.com>
2024-01-23 09:20:49 -0500

    Bound Vampire to Ground

--- Assets/VampireControl.cs
+++ Assets/VampireControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class VampireControl : MonoBehaviour
 {
     public List<GameObject> vampires = new List<GameObject>();
+    public GameObject ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,10 @@ public class VampireControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        vampires[0].transform.Translate(Vector3.right * Time.deltaTime);
+        if(vampires[0].transform.position.x < ((ground.transform.localScale.x / 2) + ground.transform.position.x))
+        {
+            vampires[0].transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        
     }
 }
